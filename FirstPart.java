0ad8fd8afb4ccf00861cec28b51029fe0de5b15e
voHAHAHA <sv.vov4ik@gmail.com>
2023-02-10 17:04:55 +0600

    'said'

---
+++ FirstPart.java
@@ -0,0 +1,8 @@
+public class FirstPart {
+    public static void main(String[] args) {
+        int[] array = {12,32,32,123};
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+}
