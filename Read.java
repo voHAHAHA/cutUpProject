9e2272687be060c05b90d4e981f774bafd1dff5f
voHAHAHA <sv.vov4ik@gmail.com>
2023-02-10 18:08:22 +0600

    'Write/Read'

--- Read.java
+++ Read.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Read {
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Read {
+    public static void main(String[] args) {
+        try (FileInputStream fis = new FileInputStream("D://9Работа с файлами//Hello.txt")) {
+            System.out.printf("File size: %d bytes \n", fis.available());
+
+            int i = -1;
+            while((i = fis.read()) != -1) {
+                System.out.println((char)i);
+            }
+        } catch (IOException exe){
+            System.out.println(exe.getMessage());
+        }
+    }
 }
--- Write.java
+++ Write.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Write {
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Write {
+    public static void main(String[] args) {
+        String text = "Hello world";
+        try (FileOutputStream fos = new FileOutputStream("D://9Работа с файлами//Hello.txt")){
+
+            byte[] buffer = text.getBytes();
+
+            fos.write(buffer, 0, buffer.length);
+            System.out.println("The file has been written");
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 }
